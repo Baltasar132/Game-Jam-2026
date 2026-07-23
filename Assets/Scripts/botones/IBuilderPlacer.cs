06dfe24154cf9e4864bff36065cc918d12f07475
Carlenyan <kako3000@gmail.com>
2026-07-22 22:45:33 -0400

    Poner estructuras, me quiero morir

--- Assets/Scripts/botones/IBuilderPlacer.cs
+++ Assets/Scripts/botones/IBuilderPlacer.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public interface IBuilderPlacer
 {
     GameObject PlaceBuild(Vector3 at);
+    Vector3 SnapToGrid(Vector3 at);
+
+    bool CanPlace(Vector3 at);
+
+    float PlaneHeight()
+    {
+        return 0.5f;
+    }
 }
