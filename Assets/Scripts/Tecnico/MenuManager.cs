ad69277989180ba6e94d5c4d3d64949f3f695a0f
PapitoPapa <santiabadin@gmail.com>
2026-07-25 01:39:51 -0300

    Assets y demás mierdas

--- Assets/Scripts/Tecnico/MenuManager.cs
+++ Assets/Scripts/Tecnico/MenuManager.cs
@@ -1,18 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+    public GameObject AnimationHolder;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(StartGameSeq());
+        AnimationHolder.SetActive(true);
+        
     }
+
+    
 
     // Update is called once per frame
     public void QuitGame()
     {
         Debug.Log("saliendo");
         Application.Quit();
+    }
+
+    IEnumerator StartGameSeq()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 }
